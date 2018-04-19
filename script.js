d027916fae0da341339979d415baeefab753a8e5
chiaqingwu <10446006@ntub.com.tw>
2018-04-19 23:31:35 +0800

    v-Bind, v-If

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 var app = new Vue({
-  el: '#app',
+  el: '#helloWorld',
   data: {
     message: 'Hello Vue ^^'
+  }
+})
+
+var app2 = new Vue({
+  el: '#vBind',
+  data: {
+    message: '滑鼠停留於 ' + new Date()
+  }
+})
+
+var app3 = new Vue({
+  el: '#vIf',
+  data: {
+    seen: true
   }
 })
