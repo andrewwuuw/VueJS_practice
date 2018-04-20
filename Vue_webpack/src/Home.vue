53dad277f656f3a2d60242a4d748372d187f545b
chiaqingwu <10446006@ntub.com.tw>
2018-04-20 11:40:37 +0800

    update App.vue, main.js, and new Home.vue

---
+++ Vue_webpack/src/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    Lotery: {{ number }}
+    <button @click="changeNumber">add 100</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      number: 100
+    }
+  },
+  methods: {
+    changeNumber() {
+      this.number += 100
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
