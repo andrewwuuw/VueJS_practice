bcc74635bec4f62b56bfbfc8b0f97851a36845bf
chiaqingwu <10446006@ntub.com.tw>
2018-04-20 09:57:31 +0800

    0420 new mount

--- 0420/vue.js
+++ 0420/vue.js
@@ -2,5 +2,21 @@ new Vue({
   el: '#app1',
   data: {
     message: 'hello, world!'
+  },
+  methods:{
+    changeWord: function(e){
+      e.target.innerText = "you";
+    },
+    changeWord2: function(e){
+      this.$refs.my_button.innerText = "Ken";
+    }
   }
 })
+
+var vm1 = new Vue({
+  data: {
+    message: 'mount!'
+  }
+})
+
+vm1.$mount('#app3')
